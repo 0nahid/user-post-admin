89cbc77fa4fb0ec58abb3ad5e12157fd6db3dd6a
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2021-12-14 02:38:52 +0600

    added firebase

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBftAqaPztIJMOXn7HTFvZQeekclJfL_1M",
+    authDomain: "user-post-admin.firebaseapp.com",
+    projectId: "user-post-admin",
+    storageBucket: "user-post-admin.appspot.com",
+    messagingSenderId: "374987762210",
+    appId: "1:374987762210:web:d5b29dabbbe61d1facf87b",
+    measurementId: "G-CT9YTZXLDM"
+};
+export default firebaseConfig;  
