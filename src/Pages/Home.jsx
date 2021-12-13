5774bc8f2e6780a10d8b70d30fce1f1044463c12
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2021-12-14 02:52:53 +0600

    added test page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,10 @@
 export default function Home() {
-    return (
-        <div>
-            <div className="text-center text-6xl text-teal-800">This is Home</div>
-        </div>
-    )
+  return (
+    <div>
+      <div className="text-center text-6xl text-red-600">
+        <h1>The site is in development mode</h1>
+        <h1>This is a test page...</h1>
+      </div>
+    </div>
+  );
 }
